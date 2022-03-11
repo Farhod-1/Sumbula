8b05f3780368d74bbb0993909f7f73a0927dc1d0
Farhod Qarshiyev <farhodqarshiyev2@gmail.com>
2022-03-12 00:30:08 +0500

    Components added

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,40 @@
+import classes from "./AvailableMeals.module.css"
+
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Somsa',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Shashlik',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Pizza',
+      description: 'Healthy...and green...',
+      price: 18.99,
+    },
+  ];
+
+const AvailableMeals = () =>{
+    const mealsList=DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+
+    return <section className={classes.meals}>
+        <ul>
+            {mealsList}
+        </ul>
+    </section>
+};
+
+export default AvailableMeals;
